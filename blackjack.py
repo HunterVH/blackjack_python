dec41b6280e5bfb686c2da3c5f3d0890d3cd8697
HunterVH <152095902+HunterVH@users.noreply.github.com>
2025-01-14 10:19:21 -0700

    Update blackjack.py
    Added functionality to do the initial deal to a single player and the dealer. I also added the outline of the hit/stay and deal/leave functionality in loops so the user can interact with the game and leave the game eventually.

--- blackjack.py
+++ blackjack.py
@@ -16,12 +16,75 @@ def createShoe(shoeSize):
 
     return deck
 
+def firstDeal(playerHand, dealerHand, shoe):
+    playerTotal = 0
+    dealerTotal = 0
+    for j in range(4):
+        card = shoe.pop()
+        cardValue = int(card[1:])
+        if(cardValue > 10):
+            if(j%2 == 1):
+                playerTotal += 10
+                playerHand.append(card)
+            else:
+                dealerTotal += 10
+                dealerHand.append(card)
+        elif(cardValue == 1):
+            if(j%2 == 1):
+                playerTotal += 11
+                playerHand.append(card)
+            else:
+                dealerTotal += 11
+                dealerHand.append(card)
+        else:
+            if(j%2 == 1):
+                playerTotal += cardValue
+                playerHand.append(card)
+            else:
+                dealerTotal += cardValue
+                dealerHand.append(card)
+
+    return playerTotal, dealerTotal
+
+
 def main():
     shoeSize = 2
-    shoe = createShoe(shoeSize)
-    print(shoe)
+    playerHand = []
+    playerTotal = 0
+    dealerHand = []
+    dealerTotal = 0
+    playing = True
 
-    pass
+    shoe = createShoe(shoeSize)
+    #print(shoe)
+
+    while(playing):
+        playerTotal, dealerTotal = firstDeal(playerHand, dealerHand, shoe)
+        print('Your Hand:', playerHand,'\nTotal:',playerTotal)
+        print('Dealer Hand:', dealerHand[0], '\nTotal:',dealerTotal)
+
+        #print('\nHit or Stay?')
+        #Get User Input
+        userIn = ''
+        while(userIn != 'Stay'):
+            userIn = input("Hit or Stay: ")
+            if(userIn == 'Hit'):
+                print('Hit!')
+            elif(userIn == 'Stay'):
+                print('Stay!')
+            else:
+                print('That was not a valid input.')
+
+        while(userIn != 'Deal' and userIn != 'Leave'):
+            userIn = input('Deal or Leave: ')
+            if(userIn == 'Leave'):
+                playing = False
+            elif(userIn != 'Deal'):
+                print('That was not a valid input')
+            else:
+                playerHand.clear()
+                dealerHand.clear()
+
 
 if __name__ == "__main__":
     main()
