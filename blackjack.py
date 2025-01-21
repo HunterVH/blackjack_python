d631bd7b28fa0c9097af3c91c18a684e7dae4068
HunterVH <152095902+HunterVH@users.noreply.github.com>
2025-01-21 09:40:23 -0700

    Add files via upload
    These files changed the program to implement an object oriented approach. I added the card, deck, and player classes and added basic functionality.

--- blackjack.py
+++ blackjack.py
@@ -2,88 +2,77 @@
 This Program will simulate a game of blackjack
 """
 
-import random
+import deck
+import player
 
-def createShoe(shoeSize):
-    deck = []
-    for iter in range(13*shoeSize):
-        deck.append('H'+str(iter%13+1))
-        deck.append('S'+str(iter%13+1))
-        deck.append('D'+str(iter%13+1))
-        deck.append('C'+str(iter%13+1))
+# def firstDeal(playerHand, dealerHand, shoe):
+#     playerTotal = 0
+#     dealerTotal = 0
+#     for j in range(4):
+#         card = shoe.pop()
+#         cardValue = int(card[1:])
+#         if(cardValue > 10):
+#             if(j%2 == 1):
+#                 playerTotal += 10
+#                 playerHand.append(card)
+#             else:
+#                 dealerTotal += 10
+#                 dealerHand.append(card)
+#         elif(cardValue == 1):
+#             if(j%2 == 1):
+#                 playerTotal += 11
+#                 playerHand.append(card)
+#             else:
+#                 dealerTotal += 11
+#                 dealerHand.append(card)
+#         else:
+#             if(j%2 == 1):
+#                 playerTotal += cardValue
+#                 playerHand.append(card)
+#             else:
+#                 dealerTotal += cardValue
+#                 dealerHand.append(card)
 
-    random.shuffle(deck)
-
-    return deck
-
-def firstDeal(playerHand, dealerHand, shoe):
-    playerTotal = 0
-    dealerTotal = 0
-    for j in range(4):
-        card = shoe.pop()
-        cardValue = int(card[1:])
-        if(cardValue > 10):
-            if(j%2 == 1):
-                playerTotal += 10
-                playerHand.append(card)
-            else:
-                dealerTotal += 10
-                dealerHand.append(card)
-        elif(cardValue == 1):
-            if(j%2 == 1):
-                playerTotal += 11
-                playerHand.append(card)
-            else:
-                dealerTotal += 11
-                dealerHand.append(card)
-        else:
-            if(j%2 == 1):
-                playerTotal += cardValue
-                playerHand.append(card)
-            else:
-                dealerTotal += cardValue
-                dealerHand.append(card)
-
-    return playerTotal, dealerTotal
+#     return playerTotal, dealerTotal
 
 
 def main():
-    shoeSize = 2
-    playerHand = []
-    playerTotal = 0
-    dealerHand = []
-    dealerTotal = 0
+    shoe = deck.deck(5)
+    print(shoe)
+    player1 = player.player('Hunter')
+    dealer = player.player('Dealer')
     playing = True
 
-    shoe = createShoe(shoeSize)
-    #print(shoe)
+    
 
     while(playing):
-        playerTotal, dealerTotal = firstDeal(playerHand, dealerHand, shoe)
-        print('Your Hand:', playerHand,'\nTotal:',playerTotal)
-        print('Dealer Hand:', dealerHand[0], '\nTotal:',dealerTotal)
+        
+        dealer.hand, player1.hand = shoe.blackjackFirstDeal()
+        dealer.update()
+        player1.update()
 
-        #print('\nHit or Stay?')
-        #Get User Input
         userIn = ''
-        while(userIn != 'Stay'):
-            userIn = input("Hit or Stay: ")
-            if(userIn == 'Hit'):
+        while(userIn[:1] != 'S' and userIn[:1] != 'Q'):
+            userIn = (input("(H)it, (S)tay, or (Q)uit: ")).upper()
+            print(userIn)
+            print(userIn[:1])
+            if(userIn[:1] == 'H'):
                 print('Hit!')
-            elif(userIn == 'Stay'):
+            elif(userIn[:1] == 'S'):
                 print('Stay!')
+            elif(userIn[:1] == 'Q'):
+                print('Quitter!')
             else:
                 print('That was not a valid input.')
 
-        while(userIn != 'Deal' and userIn != 'Leave'):
-            userIn = input('Deal or Leave: ')
-            if(userIn == 'Leave'):
+        while(userIn != 'D' and userIn != 'Q'):
+            userIn = input('(D)eal or (Q)uit: ').upper()
+            if(userIn[:1] == 'Q'):
                 playing = False
-            elif(userIn != 'Deal'):
-                print('That was not a valid input')
+            elif(userIn[:1] != 'D'):
+                pass
             else:
-                playerHand.clear()
-                dealerHand.clear()
+                print('That was not a valid input')
 
 
 if __name__ == "__main__":
