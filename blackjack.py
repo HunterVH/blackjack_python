a7842374b0deac2d341c2e4f27d5bb8c74d33821
HunterVH <hunter.vanhorn76@gmail.com>
2025-02-02 13:52:50 -0700

    Added the functionality for the dealer to hit/stay, the dealer hits on a soft 17.

--- blackjack.py
+++ blackjack.py
@@ -5,9 +5,35 @@ This Program will simulate a game of blackjack
 import deck
 import player
 
-def printHandContent(player1, dealer):
-        print(f'{player1.name}: {player1.handContent()}\n\tTotal: {player1.handValue()}\n')
-        print(f'{dealer.name}: {dealer.dealerContent()}, *')
+def printHandContentPlayerView(player1, dealer):
+    print(f'{player1.name}: {player1.handContent()}\n\tTotal: {player1.handValue()}\n')
+    print(f'{dealer.name}: {dealer.dealerContent()}, *')
+
+def printHandContentDealer(dealer):
+    print(f'{dealer.name}: {dealer.handContent()}\n\tTotal: {dealer.handValue()}\n')
+
+def dealerPlays(dealer, shoe):
+    SOFTHIT = 17
+    value = dealer.handValue()
+    # This will happen if the dealer has an ace
+    while(True):
+        if(type(value) is list):
+            if(value[1] <= 17):
+                dealer.hit(shoe.hit())
+                value = dealer.handValue()
+                # # Check if value is still a list to avoid a type error
+                # if(value is not list):
+                #     break
+            else:
+                break
+        else:
+            if value <= 16:
+                dealer.hit(shoe.hit())
+                value = dealer.handValue()
+            else:
+                break
+    printHandContentDealer(dealer)
+    
 
 def playerPlays(player1, dealer, shoe):
     playing = True
@@ -16,7 +42,7 @@ def playerPlays(player1, dealer, shoe):
         
         dealer.playerHand, player1.playerHand = shoe.blackjackFirstDeal()
 
-        printHandContent(player1, dealer)
+        printHandContentPlayerView(player1, dealer)
 
         userIn = ''
         while(userIn[:1] != 'S' and userIn[:1] != 'Q' and not player1.checkBust()):
@@ -24,7 +50,7 @@ def playerPlays(player1, dealer, shoe):
             if(userIn[:1] == 'H'):
                 print('Hit!')
                 player1.hit(shoe.hit())
-                printHandContent()
+                printHandContentPlayerView(player1, dealer)
                 if(player1.checkBust()):
                     print('You Busted!')
             elif(userIn[:1] == 'S'):
@@ -34,6 +60,8 @@ def playerPlays(player1, dealer, shoe):
                 playing = False
             else:
                 print('That was not a valid input.')
+
+        dealerPlays(dealer, shoe)
 
         while(userIn[:1] != 'D' and userIn[:1] != 'Q'):
             userIn = input('(D)eal or (Q)uit: ').upper()
@@ -54,11 +82,6 @@ def main():
     dealer = player.player('Dealer')
 
     playerPlays(player1, dealer, shoe)
-
-    
-
-
-
 
 if __name__ == "__main__":
     main()
