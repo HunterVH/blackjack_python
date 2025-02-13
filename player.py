e38e1dc018d3a1abcfb78b018d0d30e315b958a3
HunterVH <hunter.vanhorn76@gmail.com>
2025-02-13 09:36:35 -0700

    Changed the function comments from single line to docstrings.

--- player.py
+++ player.py
@@ -9,7 +9,9 @@ class player:
         self.__dict__['playerHand'] = hand.hand()
         self.busted = False
 
-    # Defines the behavior when setting class attributes
+    '''
+    Defines the behavior when setting class attributes
+    '''
     def __setattr__(self, name, value):
         if(name == 'playerHand'):
             if(type(value) is list):
@@ -23,31 +25,45 @@ class player:
         else:
             print('You cannot set this attribute')
 
-    # Returns the cards of the players hand as a comma seperated string
+    '''
+    Returns the cards of the players hand as a comma seperated string
+    '''
     def handContent(self):
         contents = ''
         for j in self.playerHand.cards:
             contents += j.name + ', '
         return contents[:-2]
     
-    # Returns the first card of the players hand as a string
+    '''
+    Returns the first card of the players hand as a string
+    '''
     def dealerContent(self):
         return self.playerHand.cards[0].name
     
-    # Returns the total value of the player's hand
+    '''
+    Returns the total value of the player's hand
+    '''
     def handValue(self):
         return self.playerHand.getHandValue()
     
-    # Adds a new card to the player's hand
+    '''
+    Adds a new card to the player's hand
+    '''
     def hit(self, newCard):
         self.playerHand.hit(newCard)
         if self.playerHand.totalValue > 21:
             self.busted = True
 
-    # Empties the player's hand of cards and resets the value
+    '''
+    Empties the player's hand of cards and resets the value
+    '''
     def clearHand(self):
         self.playerHand.fold()
+        self.busted = False
 
+    '''
+    Returns a value that determines if the player has busted
+    '''
     def checkBust(self):
         return self.busted
     
