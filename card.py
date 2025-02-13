38b6718bdba97653e3946dbe52887b28956514b5
HunterVH <hunter.vanhorn76@gmail.com>
2025-02-13 09:29:26 -0700

    Added docstrings to define the functionality of the class functions.

--- card.py
+++ card.py
@@ -2,12 +2,20 @@
 This is a class definition for the card class.
 '''
 
+'''
+An object to represent playing cards
+'''
 class card:
+    '''
+    Creates the card object
+    '''
     def __init__(self, name, suit, value):
         self.name = name
         self.suit = suit
         self.value = value
 
-    # Returns the name of the card as a string with a letter/number and suit
+    '''
+    Returns the name of the card
+    '''
     def __str__(self):
         return self.name
